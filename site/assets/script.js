08c916cd861262627e547f5069bf60ee126e9c79
Vitor Tavares <vitor.tavares.leite@gmail.com>
2024-03-10 20:05:47 -0300

    a

---
+++ site/assets/script.js
@@ -0,0 +1,16 @@
+function enviarEmail(inputId){
+ 
+ document.getElementById(inputId);
+ onclick= document.getElementById('emailtext2').value = ''
+
+ alert('Enviado!') ;
+ setInterval(count,1);
+ 
+ 
+ 
+ 
+ 
+
+
+ 
+}
